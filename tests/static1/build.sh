5fb3bef02e1b2c58944497c10ce3ec26eb05380e
Raiki Tamura <tamaron1203@gmail.com>
2023-01-18 23:20:40 +0900

    fix tests

--- tests/static1/build.sh
+++ tests/static1/build.sh
@@ -1,13 +1,7 @@
 cd $(dirname $0)
-
 LD="../../build/myld"
 
-cc a.c -S -o a.s -m64 -fno-asynchronous-unwind-tables -g0
-cc b.c -S -o b.s -m64 -fno-asynchronous-unwind-tables -g0
-
-as -c a.s -o a.o --noexecstack
-as -c b.s -o b.o --noexecstack
-
+cc a.c -c -o a.o -m64 -fno-asynchronous-unwind-tables -g0
+cc b.c -c -o b.o -m64 -fno-asynchronous-unwind-tables -g0
 $LD a.o b.o -o myld-a.out >bulid.log
-
 chmod +x myld-a.out 2>/dev/null
--- tests/static1/ld-build.sh
+++ tests/static1/ld-build.sh
@@ -1,13 +1,7 @@
 cd $(dirname $0)
-
 LD="ld"
 
-cc a.c -S -o a.s -m64 -fno-asynchronous-unwind-tables -g0
-cc b.c -S -o b.s -m64 -fno-asynchronous-unwind-tables -g0
-
-as -c a.s -o a.o --noexecstack
-as -c b.s -o b.o --noexecstack
-
+cc a.c -c -o a.o -m64 -fno-asynchronous-unwind-tables -g0
+cc b.c -c -o b.o -m64 -fno-asynchronous-unwind-tables -g0
 $LD a.o b.o -o ld-a.out -T static1.ld -nostdlib
-
 chmod +x ld-a.out 2>/dev/null

e9c38d07d77b4eb3063247c9cf5b87ba6c5eb422
Raiki Tamura <tamaron1203@gmail.com>
2023-01-18 18:19:24 +0900

    fix tests

--- tests/simple2/simple2.c
+++ tests/simple2/simple2.c
@@ -6,13 +6,10 @@ void f() {
     __builtin_unreachable();
 }
 
-// Tell the compiler incoming stack alignment is not RSP%16==8 or ESP%16==12
 __attribute__((force_align_arg_pointer)) void _start() {
-    /* main body of program: call main(), etc */
-
     f();
 
-    /* exit system call */
+    // exit(42)
     asm("mov $60, %rax");
     asm("mov $42, %rdi");
     asm("syscall");
